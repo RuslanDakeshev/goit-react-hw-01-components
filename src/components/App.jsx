557a66e36f9f8c899025e2a5b907804cd7a91634
RuslanDakeshev <bil08@outlook.com>
2022-11-19 00:04:55 +0100

    friends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,15 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+
 import user from 'User/user.json';
 import data from 'Data/data.json';
+import friends from 'Friends/friends.json';
+
 
 export const App = () => {
   return (
     <>
-      
       <Profile
         username={user.username}
         tag={user.tag}
@@ -16,7 +19,10 @@ export const App = () => {
       />
 
       <Statistics title="Upload stats" stats={data} />
-<Statistics stats={data} />
+      <Statistics stats={data} />
+
+      <FriendList friends={friends} />;
     </>
+
   );
 };
